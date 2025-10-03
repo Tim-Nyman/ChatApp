afc3ce71edfb230999987bd887af89d07c40f768
Tim Nyman <timnyman@live.com>
2025-10-03 10:46:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using ChatApp.Components;
+using ChatApp.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.MapBlazorHub();
+app.MapHub<ChatHub>("/chathub");
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
