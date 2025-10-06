83afe3799a9e8918f467b5210438821e39333b62
Tim Nyman <timnyman@live.com>
2025-10-06 13:26:13 +0200

    #refactored code, added service and decoupled chat and chatwindow from signalR.

--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
 using ChatApp.Components;
 using ChatApp.Hubs;
+using ChatApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSignalR();
 
-
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<ChatService>();
 
 var app = builder.Build();
 
@@ -23,7 +25,6 @@ app.UseRouting();
 app.UseHttpsRedirection();
 app.MapHub<ChatHub>("/chatHub");
 app.UseAntiforgery();
-
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
